fca69af36fa1dcc79614e34f8714f06784165f9c
Paul Robert Lalo <pml8@njit.edu>
2016-12-06 22:43:56 -0500

    Step 3 is working, Step 4 is partially working (for cases .contains(s), removes EVERY word with s).

--- src/hifidse.java
+++ src/hifidse.java
@@ -146,7 +146,7 @@ public class hifidse{
 	//Write method
 	public static void writeFile(BufferedWriter bwout, String bwin, File fin) throws IOException {
 			
-		bwout.write("("+ fin + ", " + bwin + ", " + wordOffset + ", " + "0)"); //write in the order specified (docURL, word, offset, attrVal)
+		bwout.write("("+ fin + "," + bwin + "," + wordOffset + "," + "0)"); //write in the order specified (docURL, word, offset, attrVal)
 		bwout.newLine();
 		wordOffset++; //plus 1 after every word
 	}
@@ -155,11 +155,95 @@ public class hifidse{
 	}
  
 	public static void stopword(){
+		try{
+			token(); 
+			 
+			//writer
+			File fout = new File(path + "\\step3.txt"); //Specify output file
+			FileOutputStream fos = new FileOutputStream(fout);
+			BufferedWriter bwout = new BufferedWriter(new OutputStreamWriter(fos)); 
+			 
+			//input file
+			File fin = new File(path + "\\step2.txt");
+			 
+			//read file
+			readFileStopwords(fin, bwout); 
+			bwout.close();
+		}
+		catch (Exception e){
+			 
+		}
 	}
- 
+	
+	//Read/Write method - for stopwords, read previous step instead of original file.
+	public static void readFileStopwords(File fin, BufferedWriter bwout) throws IOException{
+		Scanner br = new Scanner(new File(fin.toString())); //reading the file
+		while (br.hasNext()){
+			String[] tokens = br.next().split("\\r\\n|\\n|\\r"); //split document by newline
+			for (int i = 0; i < tokens.length; i++){
+				if (!tokens[i].contains("i,") && !tokens[i].contains("a,") && !tokens[i].contains("about,") && !tokens[i].contains("an,") && !tokens[i].contains("are,") && !tokens[i].contains("as,") && !tokens[i].contains("at,") && !tokens[i].contains("be,") && !tokens[i].contains("by,") && !tokens[i].contains("com,") && !tokens[i].contains("en,") && !tokens[i].contains("for,") && !tokens[i].contains("from,") && !tokens[i].contains("how,") && !tokens[i].contains("in,") && !tokens[i].contains("is,") && !tokens[i].contains("it,") && !tokens[i].contains("of,") && !tokens[i].contains("on,") && !tokens[i].contains("or,") && !tokens[i].contains("that,") && !tokens[i].contains("the,") && !tokens[i].contains("this,") && !tokens[i].contains("to,") && !tokens[i].contains("was,") && !tokens[i].contains("what,") && !tokens[i].contains("when,") && !tokens[i].contains("where,") && !tokens[i].contains("who,") && !tokens[i].contains("will,") && !tokens[i].contains("with,") && !tokens[i].contains("www,")){
+					bwout.write(tokens[i]); //convert to lower case and write to output file.
+					bwout.newLine();
+				}
+			}
+		}
+		br.close();
+	}
+	
+	//Stem method 
 	public static void stem(){
+		try{
+			stopword(); 
+			 
+			//writer
+			File fout = new File(path + "\\step4.txt"); //Specify output file
+			FileOutputStream fos = new FileOutputStream(fout);
+			BufferedWriter bwout = new BufferedWriter(new OutputStreamWriter(fos)); 
+			 
+			//input file
+			File fin = new File(path + "\\step3.txt");
+			 
+			//read file
+			readFileStem(fin, bwout); 
+			bwout.close();
+		}
+		catch (Exception e){
+			 
+		}
 	}
- 
+	
+	//Read/Write method - for stemming, read previous step instead of original file.
+	public static void readFileStem(File fin, BufferedWriter bwout) throws IOException{
+		Scanner br = new Scanner(new File(fin.toString())); //reading the file
+
+		while (br.hasNext()){
+			String[] tokens = br.next().split("\\r\\n|\\n|\\r"); //split document by spaces
+			for (int i = 0; i < tokens.length; i++){
+				if (tokens[i].contains("ies,") && !tokens[i].contains("eies,") && !tokens[i].contains("aies,")){
+					String stemToken = tokens[i].replaceAll("ies", "y");
+					bwout.write(stemToken);
+					bwout.newLine();
+				}
+				else if (tokens[i].contains("es,") && !tokens[i].contains("aes,") && !tokens[i].contains("ees,") && !tokens[i].contains("oes,")){
+					String stemToken = tokens[i].replaceAll("es", "e");
+					bwout.write(stemToken); 
+					bwout.newLine();
+				}
+				else if (tokens[i].contains("s,") && !tokens[i].contains("us,") && !tokens[i].contains("ss,")){
+					String stemToken = tokens[i].replaceAll("s", "");
+					bwout.write(stemToken); 
+					bwout.newLine();
+				}
+				else
+				{
+					bwout.write(tokens[i]);
+					bwout.newLine();
+				}
+			}
+		}
+		br.close();
+	}
+	 
 	public static void stemdebug(){
  	}
  
