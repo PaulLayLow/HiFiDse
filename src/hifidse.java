21a6a0533a3fd8ee3991e7232775217bdfc62043
Paul Robert Lalo <pml8@njit.edu>
2016-12-08 22:30:03 -0500

    Fixed tokenizer to tokenize every file in directory rather than one file at a time. Also fixed "token" to show docId and tokendebug as docUrl.

--- src/hifidse.java
+++ src/hifidse.java
@@ -14,6 +14,7 @@ public class hifidse{
 	public static int dirCounter = 1; //counter for directory/files
 	public static String[] txtExtensions = {".html" , ".htm" , ".txt" , ".cc" , ".cpp" , ".c" , ".h" , ".java"}; //text-readable extensions
 	public static int wordOffset = 0; //Keeps position of words in the text
+	public static int docId = 0; //Assigns a docID to a docUrl
 	final static String path = System.getProperty("user.dir"); //Get directory currently in
  
 	public static void main(String[] args){
@@ -33,14 +34,30 @@ public class hifidse{
        
 			//Switching to different methods
 			switch (arguement){
-				case "searchable": searchable();
-			 	case "token": token();
-			 	case "tokendebug": tokendebug();
-			 	case "stopword": stopword();
-			 	case "stem": stem();
-			 	case "stemdebug": stemdebug();
-			 	case "invert": invert();
-			 	default: queryEngine();      
+			case "searchable": 
+				searchable();
+				break;
+		 	case "token": 
+		 		token();
+		 		break;
+		 	case "tokendebug": 
+		 		tokendebug();
+		 		break;
+		 	case "stopword": 
+		 		stopword();
+		 		break;
+		 	case "stem": 
+		 		stem();
+		 		break;
+		 	case "stemdebug": 
+		 		stemdebug();
+		 		break;
+		 	case "invert": 
+		 		invert();
+		 		break;
+		 	default:
+		 		queryEngine(); 
+		 		break;      
 			} 
 		}
     
@@ -117,41 +134,87 @@ public class hifidse{
 			File fout = new File(path + "\\step2.txt"); //Specify output file
 			FileOutputStream fos = new FileOutputStream(fout);
 			BufferedWriter bwout = new BufferedWriter(new OutputStreamWriter(fos)); 
-			 
+			wordOffset = 0; 
 			//input file
-			File fin = new File(path + "\\" +name);
+			File[] dir = new File(name).listFiles();
+			showFilesToken(dir, bwout, false);
 			 
-			//read file
-			readFile(fin, bwout); 
+			//close file
 			bwout.close();
 		}
 		catch (Exception e){
 			 System.out.println("Error occured while trying to tokenize file");
 		}
 	}
-	 
+	
+	//Recursive method to go through files for token
+	public static void showFilesToken(File[] files, BufferedWriter bwout, boolean debug) {         
+		try{
+			for (File file : files) {
+	        
+				//if a directory
+				if (file.isDirectory()) {
+					showFilesToken(file.listFiles(), bwout, debug); // Calls same method again.
+				} 
+	          
+				//if a file
+				else {
+					String fullPath = file.getAbsolutePath(); //only process text-readable files
+					if (Arrays.asList(txtExtensions).contains(fullPath.substring(fullPath.indexOf("."), fullPath.length()))){
+						readFile(file, bwout, debug);
+					}
+				}
+			}
+		}
+	    
+		catch (Exception e){
+			System.out.println("Error occured while listing files in directory");
+		}
+	}	 
+		
 	//Read method
-	public static void readFile(File fin, BufferedWriter bwout) throws IOException{
+	public static void readFile(File fin, BufferedWriter bwout, boolean debug) throws IOException{
 		Scanner br = new Scanner(new File(fin.toString())); //reading the file
 			
 		while (br.hasNext()){
 			String[] tokens = br.next().split("\\s+"); //split document by spaces
 			for (int i = 0; i < tokens.length; i++){
-				writeFile(bwout, tokens[i].toLowerCase(), fin); //convert to lower case and write to output file.
+				writeFile(bwout, tokens[i].toLowerCase(), fin, debug); //convert to lower case and write to output file.
 			}
 		}
+		docId++;
 		br.close();
 	}
 	 
 	//Write method
-	public static void writeFile(BufferedWriter bwout, String bwin, File fin) throws IOException {
-			
-		bwout.write("("+ fin + "," + bwin + "," + wordOffset + "," + "0)"); //write in the order specified (docURL, word, offset, attrVal)
+	public static void writeFile(BufferedWriter bwout, String bwin, File fin, boolean debug) throws IOException {
+		if (debug){	
+			bwout.write("("+ fin + "," + bwin + "," + wordOffset + "," + "0)"); //write in the order specified (docURL, word, offset, attrVal)
+		}
+		else{
+			bwout.write("("+ docId + "," + bwin + "," + wordOffset + "," + "0)");
+		}
 		bwout.newLine();
 		wordOffset++; //plus 1 after every word
 	}
 
 	public static void tokendebug(){
+		try{	 
+			//writer
+			File fout = new File(path + "\\step2d.txt"); //Specify output file
+			FileOutputStream fos = new FileOutputStream(fout);
+			BufferedWriter bwout = new BufferedWriter(new OutputStreamWriter(fos)); 
+			wordOffset = 0; 
+			//input file
+			File[] dir = new File(name).listFiles();
+			showFilesToken(dir, bwout, true);
+			 
+			//close file
+			bwout.close();
+		}
+		catch (Exception e){
+			 System.out.println("Error occured while trying to tokenize file");
+		}
 	}
  
 	public static void stopword(){
